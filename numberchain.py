289513c6c59c6100222eb4b2a170e360eea79afa
Roland Angerer <r.angerer@yelsterdigital.com>
2016-03-31 08:48:21 +0200

    add coding dojo outcome

--- numberchain.py
+++ numberchain.py
@@ -1,8 +1,38 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-def numberchain():
-    pass
+from functools import partial
+from itertools import permutations
+
+
+def determine_key(val, multiplier):
+    val_as_string = str(val)
+    return val_as_string * (multiplier / len(val_as_string) + 1)
+    
+
+def numberchain(*numbers):
+    """Arrange non-negative integers to build the largest possible number."""
+    multiplier = len(str(max(numbers)))
+    key_function = partial(determine_key, multiplier=multiplier)
+    sorted_numbers = reversed(sorted(numbers, key=key_function))
+    return int("".join(str(n) for n in sorted_numbers))
+
+
+def correct_solution(*numbers):
+    highscore = 0
+    for permutation in permutations(numbers):
+        score = int(''.join(str(n) for n in permutation))
+        if score > highscore:
+            highscore = score
+    return highscore
+    
+    
+def reference_implementation(*numbers):
+    def comp_func(a, b):
+        return cmp(str(a)+str(b), str(b)+str(a))
+    sorted_numbers = reversed(sorted(numbers, cmp=comp_func))
+    return int("".join(str(n) for n in sorted_numbers))
+    
 
 if __name__ == "__main__":
     import nose
--- test_numberchain.py
+++ test_numberchain.py
@@ -3,7 +3,81 @@
 
 from nose.tools import *
 
-from numberchain import numberchain
+from numberchain import numberchain, reference_implementation, correct_solution
+
 
 def test_numberchain():
-    pass
+    assert_equals(numberchain(1), 1)
+
+def test_numberchain_two_numbers():
+    assert_equals(numberchain(2,1), 21)
+
+def test_numberchain_three_numbers():
+    assert_equals(numberchain(1,1,1), 111)
+    
+def test_numberchain_4():
+    assert_equals(numberchain(1,2,3,4), 4321)
+
+def test_numberchain_5():
+    assert_equals(numberchain(10,2), 210)
+    
+def test_numberchain_6():
+    assert_equals(numberchain(20, 1), 201)
+    
+def test_numberchain_7():
+    assert_equals(numberchain(50, 2, 1, 9), 95021)
+    
+def test_numberchain_8():
+    assert_equals(numberchain(420, 42, 423), 42423420)
+    
+def test_numberchain_9():
+    assert_equals(numberchain(420, 42, 423, 4), 442423420)
+    
+def test_numberchain_10():
+    assert_equals(numberchain(420, 423, 4), 4423420)
+    
+def test_numberchain_11():
+    assert_equals(numberchain(420, 423, 4, 45), 454423420)
+    
+def test_numberchain_12():
+    assert_equals(numberchain(420, 423, 4, 450), 4504423420)
+    
+def test_numberchain_13():
+    assert_equals(numberchain(420, 423, 4, 41), 442342041)
+
+def test_numberchain_14():
+    assert_equals(numberchain(4209, 4239, 4, 414), 442394209414)
+
+def test_numberchain_15():
+    assert_equals(numberchain(420, 424, 4, 45), 454424420)
+    
+def test_numberchain_16():
+    numbers = (42424242421, 423, 425, 42)
+    assert_equals(numberchain(*numbers), correct_solution(*numbers))
+    
+def test_numberchain_17():
+    assert_equals(numberchain(42424242425, 423, 425, 42), 4254242424242542423)
+    
+def test_numberchain_18():
+    numbers = (4242424241, 423, 425, 42)
+    assert_equals(numberchain(*numbers), correct_solution(*numbers))
+    
+def test_numberchain_19():
+    assert_equals(numberchain(4242424247, 423, 425, 42), 425424242424742423)
+    
+
+def test_numberchain_really_correct_1():
+    numbers = (4242424247, 423, 425, 42)
+    assert_equals(numberchain(*numbers), correct_solution(*numbers))
+    
+def test_numberchain_really_correct_2():
+    numbers = (42423, 42425, 42)
+    assert_equals(numberchain(*numbers), correct_solution(*numbers))
+    
+def test_numberchain_really_correct_3():
+    numbers = (4242423, 4242425, 42)
+    assert_equals(numberchain(*numbers), correct_solution(*numbers))
+    
+def test_numberchain_with_reference_1():
+    numbers = (4242423, 4242425, 42)
+    assert_equals(reference_implementation(*numbers), correct_solution(*numbers))
